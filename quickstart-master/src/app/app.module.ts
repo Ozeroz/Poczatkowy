b4a4e70884dac4cdae09095df8a0432291a1f18d
Wszerad <wszerad91@gmail.com>
2017-04-07 21:25:49 +0200

    init

--- quickstart-master/src/app/app.module.ts
+++ quickstart-master/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent }  from './app.component';
+import { Page1Component } from "./page1/page1.component";
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [
+    BrowserModule
+  ],
+  declarations: [
+    AppComponent,
+    Page1Component
+  ],
+  bootstrap:    [
+    AppComponent
+  ]
 })
 export class AppModule { }
---
+++ quickstart-master/src/app/page1/page1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ts-page1',
+  templateUrl: 'app/page1/page1.template.html',
+})
+
+export class Page1Component  {
+  test: string = 'kot';
+}
+
